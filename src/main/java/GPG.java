258fb90cab0309e81f52e0f9e782f44fccb5cf69
Simon Lundström <simmel@soy.se>
2014-01-16 13:33:10 +0100

    Working GPG implementation, needs alot of fixing
    Commit early, commit often... I need a "uncommited lines" counter in Vim
    which warns me. No.. NO! Don't start on that now! AAARRRRGGGHHH

--- src/main/java/GPG.java
+++ src/main/java/GPG.java
@@ -2,6 +2,8 @@ package se.soy.gpg;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.io.*;
 
 public class GPG {
   // FIXME Remove when done
@@ -29,7 +31,83 @@ public class GPG {
   public void output() {
     println("Command: " + pre_command + command);
     println("----------");
-    gpg = null;
+
+    try {
+      List<String> commandline = new ArrayList<String>(pre_command);
+      commandline.addAll(command);
+
+      ProcessBuilder pb = new ProcessBuilder(commandline);
+
+      Process p = pb.start();
+      p.getOutputStream().write(gpg.data.getBytes());
+      p.getOutputStream().flush();
+      p.getOutputStream().close();
+
+      String line = null;
+      int code = p.waitFor();
+      if (code != 0) {
+        String exception = String.format("Exit status: %d", code);
+
+        BufferedReader stderr = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+        while ((line = stderr.readLine ()) != null) {
+          exception += "\n" + line;
+        }
+        stderr.close();
+        throw new GPGException(exception);
+      }
+
+      /* Reader stdout = new InputStreamReader(p.getInputStream()); */
+      BufferedReader stdout = new BufferedReader(new InputStreamReader(p.getInputStream()));
+      /* char buf[] = new char[1024]; */
+      /* char meow[] = new char[expectedSize]; */
+      /* int read_now = 0; */
+      /* int read_until_now = 0; */
+      /* try (SecurerString decrypted = new SecurerString(meow)) { */
+      /*   try (SecurerString ss_buf = new SecurerString(buf)) { */
+        /*   while ((read_now = stdout.read(ss_buf.chars, 0, 1024)) != -1) { */
+        /*     System.arraycopy(ss_buf.chars, 0, decrypted.chars, read_until_now, read_now); */
+        /*     read_until_now += read_now; */
+        /*   } */
+        /* } */
+        /* System.out.println(read_until_now); */
+      while ((line = stdout.readLine ()) != null) {
+        System.out.println(line);
+      }
+      stdout.close();
+
+      /*   // http://stackoverflow.com/a/5428621 */
+      /*   Class<?> c; */
+      /*   Method method; */
+      /*   try { */
+      /*     try { */
+      /*       c = (Class<?>)className; */
+      /*     } */
+      /*     catch (ClassCastException e) { */
+      /*       c = className.getClass (); */
+      /*     } */
+
+      /*     System.out.println("invoking " + c.getSimpleName () + "::[" + methodName + "]..."); */
+      /*     method = c.getDeclaredMethod(methodName, decrypted.chars.getClass()); */
+      /*     method.invoke(className, decrypted.chars); */
+      /*   } */
+      /*   // FIXME Which exceptions? */
+      /*   catch (Exception e) { */
+      /*     e.printStackTrace(); */
+      /*   } */
+      /* } */
+
+    }
+    catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+    finally {
+      gpg = null;
+    }
+  /* } */
+
   }
 
   public <T> void output(T file) {
