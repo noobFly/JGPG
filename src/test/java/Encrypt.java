ddb3a267dc1da227c7f292e2acbab4f7036bd9fb
Simon Lundström <simmel@soy.se>
2014-01-21 00:04:45 +0100

    Remove superfluous encryption test

--- src/test/java/Encrypt.java
+++
@@ -1,9 +0,0 @@
-import org.junit.*;
-import static org.junit.Assert.*;
-import se.soy.gpg.GPG;
-
-public class Encrypt extends SearchInHeap {
-  @Test public void encrypt() {
-    GPG.encrypt("really_secret_string").armor().sign().output(System.out, "println");
-  }
-}
